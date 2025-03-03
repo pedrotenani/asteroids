6150069f9de1522bdd3b0e840c2e7951e64db766
pedrotenani <pedrotenani1@gmail.com>
2025-03-03 15:32:53 -0300

    window painting black

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 import pygame
+from constants import *
 
-def main()
+def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill((0, 0, 0))  # Fill the screen with black color
+        pygame.display.flip()   # Refresh the screen
+
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
